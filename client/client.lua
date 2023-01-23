97ab558a7bedd7aca15e2296cd4aed8d70a31296
SYNO® <89760730+SYNO-SY@users.noreply.github.com>
2023-01-23 22:25:45 +0530

    Create client.lua

---
+++ client/client.lua
@@ -0,0 +1,32 @@
+local ESX = exports["es_extended"]:getSharedObject()
+local conf = Config.Discord;
+
+CreateThread(function()
+    while true do
+        SetDiscordAppId(conf.ApplicationId)
+        SetDiscordRichPresenceAsset(conf.IconLarge)
+        SetDiscordRichPresenceAssetText(conf.IconLargeHoverText)
+        SetDiscordRichPresenceAssetSmall(conf.IconSmall)
+        SetDiscordRichPresenceAssetSmallText(conf.IconSmallHoverText)
+        local playerPed = PlayerPedId()
+		    local coords = GetEntityCoords(playerPed, true)
+        local streetnamehash = GetStreetNameAtCoord(coords.x, coords.y, coords.z)
+        local location = GetStreetNameFromHashKey(streetnamehash)
+        if conf.ShowPlayerCount then
+            ESX.TriggerServerCallback('SY_Rpc:server:GetcurrentPlayers', function(result)
+                SetRichPresence('Wlaking on the '..location..'\nPlayers: ' .. result .. '/' .. conf.MaxPlayers)
+            end)
+        end
+
+        if conf.Buttons and type(conf.Buttons) == "table" then
+            for i,v in pairs(conf.Buttons) do
+                SetDiscordRichPresenceAction(i - 1,
+                    v.text,
+                    v.url
+                )       
+            end
+        end
+
+        Wait(6000)
+    end
+end)
